b2c0850733ef21ec8c5e5677741ab0a7afef8bb1
ms-kaylaa <108439098+ms-kaylaa@users.noreply.github.com>
2026-07-23 08:27:35 -0400

    clickable images

--- commissions/comms.js
+++ commissions/comms.js
@@ -1,5 +1,37 @@
 const COMMS_DIV = document.getElementById("comms")
 
+let BLANKER = undefined
+let PREVIEW_IMG = undefined
+
+function makepreview(imgpath) {
+    if(document.getElementById("blanker")) {
+        return
+    }
+
+    let madeblanker = document.createElement("div")
+        madeblanker.id = "blanker"
+
+        let madeimg = document.createElement("img")
+            madeimg.id = "preview"
+            madeimg.src = imgpath
+
+            madeimg.addEventListener("click", function() {
+                window.open(imgpath, '_blank');
+            })
+
+        madeblanker.append(madeimg)
+
+    madeblanker.addEventListener("click", function() {
+        BLANKER.remove()
+        PREVIEW_IMG.remove()
+    })
+
+    BLANKER = madeblanker
+    PREVIEW_IMG = madeimg
+
+    document.body.append(BLANKER)
+}
+
 fetch("comms.json")
     .then(response => response.json())
     .then(data => {
@@ -10,10 +42,19 @@ fetch("comms.json")
                 let images = document.createElement("div")
                     images.className = "imagerow"
                     for(const image of row.images) {
-                        let img = document.createElement("img")
-                            img.src = "img/" + image
+                        let container = document.createElement("div")
+                            container.className = "comm-img-container"
 
-                        images.append(img)
+                            let img = document.createElement("img")
+                                img.className = "comm-img"
+                                img.src = "img/" + image
+
+                                img.addEventListener("click", function() {
+                                    makepreview("img/" + image)
+                                })
+                            container.append(img)
+
+                        images.append(container)
                     } 
                 div.append(images)
 
